36f2cb869c1b9b6c3f19201dbfd6e4396c49c8fe
StrangeHum <vadim.k.2004@yandex.ru>
2024-04-09 03:44:14 +0300

    Add: Nav bar Fix: Routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,50 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  Routes,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
 import { LoginPage } from "./pages/login";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
-import { MainPage } from "./pages/MainPage";
+import { Home } from "./pages/Home";
 
 export function App() {
   return (
     <>
       <Header />
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+      {/* <RouterProvider router={router} /> */}
       <Footer />
     </>
   );
 }
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainPage />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-]);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={}>
+//       <Route index element={<Home />} />
+//       <Route path="/login" element={<LoginPage />} />,
+//     </Route>
+//   )
+// );
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <MainPage />,
+//   },
+//   {
+//     path: "/login",
+//     element: <LoginPage />,
+//   },
+// ]);
 
 export default App;
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,10 +1,34 @@
 import cn from "classnames";
 import styles from "./Header.module.scss";
+import { Link, NavLink, NavLinkProps } from "react-router-dom";
 
 export type HeaderProps = {};
 
 export const Header = ({}: HeaderProps) => {
-  return <div className={cn([styles.header, "header"])}>Header</div>;
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <NavLinkItem to="/">Home</NavLinkItem>
+        <NavLinkItem to="/login">Авторизация</NavLinkItem>
+        <NavLinkItem to="/statements">Мои заявления</NavLinkItem>
+        <NavLinkItem to="/create">Создать заявление</NavLinkItem>
+        <NavLinkItem to="/admin">Панель администратора</NavLinkItem>
+      </nav>
+    </header>
+  );
+};
+
+const navClassName = ({ isActive }: { isActive: boolean }) => {
+  return cn([styles.nav, isActive ? styles.active : ""]);
+};
+
+const NavLinkItem = (props) => {
+  const { to, children } = props;
+  return (
+    <NavLink to={to} className={navClassName}>
+      {children}
+    </NavLink>
+  );
 };
 
 export default Header;
